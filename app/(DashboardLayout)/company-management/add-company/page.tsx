7d1cd23bc5442a801d62a06a70c743885c7be4a3
wdcs-akhtarhusen <akhtarhusen@webcluesinfotech.com>
2025-07-08 20:43:29 +0530

    add comppany form  UI integration

--- app/(DashboardLayout)/company-management/add-company/page.tsx
+++ app/(DashboardLayout)/company-management/add-company/page.tsx
@@ -1,19 +1,12 @@
 'use client';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
-import { UserInfoForm } from '@/components/shared/forms/UsrinfoForm';
 import { ImageUpload } from '@/components/shared/ImageUpload';
-import { Button } from '@/components/ui/button';
 import { ChevronRight } from 'lucide-react';
+import { CompanyInfoForm } from '../../../../components/shared/forms/CompanyinfoForm';
 
 const CreateCompany = () => {
-  const router = useRouter();
   const handleUploadClick = () => {
     console.log('Upload clicked');
-  };
-  const handleCreateClick = () => {
-    router.push('/company-management/company-details');
   };
   return (
     <div className=''>
@@ -49,22 +42,7 @@ const CreateCompany = () => {
 
           {/* Right Column - Form Fields */}
           <div className='flex-1 bg-[var(--white-background)] rounded-[20px] border border-[var(--border-dark)] p-[28px]'>
-            <UserInfoForm />
-            {/* Action Buttons */}
-            <div className='flex justify-end gap-6 mt-8'>
-              <Link
-                href={'/company-management'}
-                className='inline-flex items-center h-[48px] px-8 border-2 border-[var(--border-dark)] bg-transparent rounded-full font-semibold text-[var(--text-dark)]'
-              >
-                Cancel
-              </Link>
-              <Button
-                className='h-[48px] px-12 bg-[var(--secondary)] hover:bg-[var(--hover-bg)] rounded-full font-semibold text-white'
-                onClick={handleCreateClick}
-              >
-                Create
-              </Button>
-            </div>
+            <CompanyInfoForm />
           </div>
         </div>
       </div>
